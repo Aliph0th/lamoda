9f9ac17e72e77c8fe4cb8257fb52802980d3addf
Aliph0th <theblacklord05@gmail.com>
2024-10-19 14:41:03 +0300

    feat(client): add price limits and loader to filter

--- app/client/src/App.tsx
+++ app/client/src/App.tsx
@@ -2,22 +2,27 @@ import { useEffect, useState } from 'react';
 import Filter from './components/Filter';
 import Loader from './components/ui/Loader';
 import { API } from './api';
-import { Product, ProductResponse } from 'common';
+import { MetadataResponse, Product, ProductResponse } from 'common';
 import ProductList from './components/products/ProductList';
 
 function App() {
    const [loading, setLoading] = useState<boolean>(false);
    const [products, setProducts] = useState<Product[]>([]);
    const [totalProducts, setTotalProducts] = useState<number>(0);
+   const [priceLimits, setPriceLimits] = useState<MetadataResponse['priceLimits']>([-1, -1]);
 
    useEffect(() => {
       setLoading(true);
       const fetchProducts = async () => {
-         const { data } = await API.get<ProductResponse>('/product', {
-            params: new URLSearchParams(window.location.search)
-         });
-         setProducts(data.products);
-         setTotalProducts(data.totalProducts);
+         const [productResponse, metadataResponse] = await Promise.all([
+            API.get<ProductResponse>('/product', {
+               params: new URLSearchParams(window.location.search)
+            }),
+            API.get<MetadataResponse>('/metadata')
+         ]);
+         setProducts(productResponse.data.products);
+         setTotalProducts(productResponse.data.totalProducts);
+         setPriceLimits(metadataResponse.data.priceLimits);
       };
       fetchProducts().finally(() => setLoading(false));
    }, []);
@@ -26,7 +31,7 @@ function App() {
       <main className="m-auto py-8 w-10/12">
          <h1 className="font-bold text-4xl">Lamoda</h1>
          <div className="mt-4 grid grid-cols-[2fr_5fr] gap-x-10">
-            <Filter totalProducts={totalProducts} />
+            <Filter loading={loading} totalProducts={totalProducts} priceLimits={priceLimits} />
             {loading ? <Loader /> : <ProductList products={products} />}
          </div>
       </main>
--- app/client/src/components/Filter.tsx
+++ app/client/src/components/Filter.tsx
@@ -1,18 +1,32 @@
 import { FC } from 'react';
 import MultiSlider from './ui/MultiSlider';
+import { MetadataResponse } from 'common';
+import Loader from './ui/Loader';
 
 interface FilterProps {
+   loading: boolean;
    totalProducts: number;
+   priceLimits: MetadataResponse['priceLimits'];
 }
-const Filter: FC<FilterProps> = ({ totalProducts }) => {
+const Filter: FC<FilterProps> = ({ loading, totalProducts, priceLimits }) => {
    return (
       <div className="sticky top-1 self-start">
          <input
             type="text"
-            className="mb-3 bg-white border-gray-300 text-gray-900 focus:ring-blue-200 border text-sm rounded-lg focus:ring-2 focus:outline-none w-full p-2"
+            disabled={loading}
+            className="mb-3 bg-white disabled:bg-gray-200 border-gray-300 text-gray-900 focus:ring-blue-200 border text-sm rounded-lg focus:ring-2 focus:outline-none w-full p-2"
             placeholder="Search by name or description"
          />
-         <MultiSlider minValue={0} maxValue={100} onChange={(min, max) => console.log(min, max)} />
+         {loading ? (
+            <Loader />
+         ) : (
+            <MultiSlider
+               minValue={priceLimits[0]}
+               maxValue={priceLimits[1]}
+               onChange={(min, max) => console.log(min, max)}
+            />
+         )}
+
          <span className="font-medium">Total: {totalProducts}</span>
       </div>
    );
