6a686e5da2e47a223d0b6a84aedaeb77aa7f80aa
Aliph0th <theblacklord05@gmail.com>
2024-10-27 01:17:45 +0300

    fix(client): set default backend url

--- app/client/src/api.ts
+++ app/client/src/api.ts
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export const API = axios.create({
    headers: { 'Content-Type': 'application/json' },
-   baseURL: import.meta.env.VITE_API_URL
+   baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000'
 });
